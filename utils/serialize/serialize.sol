c62bc2ce1bfb596034db9964ee1b0ed2581dd63d
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2019-01-19 12:27:16 +0100

    Rewrite example serialize function

--- utils/serialize/serialize.sol
+++ utils/serialize/serialize.sol
@@ -2,16 +2,12 @@ pragma solidity ^0.4.19;
 
 import "./Seriality.sol";
 
-contract Serialize is Seriality{
-    string [] arr = ["string0", "string1", "string2", "string3", "string4"];
+contract Serialize is Seriality {
 
-    function getBytes(uint startindex, uint endindex) public view returns(bytes serialized){
+    function getBytes(array arr) public view returns(bytes serialized){
 
-        require(endindex >= startindex);
-
-        if(endindex > (arr.length - 1)){
-            endindex = arr.length - 1;
-        }
+        uint startindex = 0;
+        uint endindex = arr.length -1;
 
         //64 byte is needed for safe storage of a single string.
         //((endindex - startindex) + 1) is the number of strings we want to pull out.
@@ -29,26 +25,6 @@ contract Serialize is Seriality{
         }
 
         return (buffer);
-    }
-
-    function getString(bytes buffer) public view returns(string string1, string string2){
-
-        //64 byte is needed for safe storage of a single string.
-        //In this example we are returning 2 strings
-        uint offset = 64*2;
-
-        buffer = new  bytes(offset);
-
-        string1 = new string(32);
-        string2 = new string(32);
-
-        bytesToString(offset, buffer, bytes(string2));
-        offset -= sizeOfString(string2);
-
-        bytesToString(offset, buffer, bytes(string2));
-        offset -= sizeOfString(string2);
-
-        return(string1, string2);
     }
 
 }
